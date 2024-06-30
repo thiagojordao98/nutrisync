4527a675aca18322315a36126a2c4451ec14bdb5
thiagojordao98 <thiagojordao98@gmail.com>
2024-06-30 07:46:06 -0300

    refactor: vercel build files

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "BUILD START"
-python3.9  -m pip install -r requirements.txt
-# python3.9 manage.py makemigrations
-# python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
+python3 pip install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
 echo "BUILD END"
