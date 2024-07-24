37f5942c5cfda36eeef4eb5e9544c16a0fd25b6d
thiagojordao98 <thiagojordao98@gmail.com>
2024-07-23 21:47:38 -0300

    buid: add new host

--- nutrisync/settings.py
+++ nutrisync/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://nutrisync.onrender.com']
+ALLOWED_HOSTS = ['https://nutrisync.onrender.com', 'localhost']
 
 # Application definition
 
