9ccfdfda8e456483bdcee975d0831cd4868ec9c8
Thiago Jordão <47632506+thiagojordao98@users.noreply.github.com>
2026-01-24 15:59:25 -0300

    Fix comment for database host in settings.py

--- nutrisync/settings.py
+++ nutrisync/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'), # Deve ser 'database_server'
+        'HOST': os.environ.get('POSTGRES_HOST'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
